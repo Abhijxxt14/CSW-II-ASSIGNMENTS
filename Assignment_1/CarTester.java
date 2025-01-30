4a6fcb78aa27778f3eb41ad497dde122e16f2df5
Abhijeet soren <154517350+Abhijxxt14@users.noreply.github.com>
2025-01-30 19:57:03 +0530

    Create CarTester.java

---
+++ Assignment_1/CarTester.java
@@ -0,0 +1,43 @@
+package assignment;
+class Car { 
+	private int make;
+	private String model;
+	public Car(int make , String model) {
+		this.make=make;
+		this.model=model;
+	}
+	public int getMake() {
+		return make;
+	}
+	public String getModel() {
+	return model;
+	}
+	public void setMake(int make) {
+		this.make=make;
+	}
+	public void setModel(String model) {
+		this.model=model;
+	}
+	public void displayd() {
+		System.out.println(make + model);
+	}
+	
+}
+
+public class CarTester {
+
+	public static void main(String[] args) {
+	Car mycar1 = new Car( 2016," ABC ");
+	Car mycar2 = new Car(0, null);
+	System.out.print("car 1 "  );
+	mycar1.displayd();
+	System.out.print("car 2 "  );
+	mycar2.displayd();
+	mycar2.setMake(20);
+	mycar2.setModel(" GHI ");
+	System.out.println("now car 2 is ");
+	mycar2.displayd();
+	
+	}
+
+}
